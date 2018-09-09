e43c883fdd9cad8a431fac99684c6a97987b9694
闫玉 <xwp_fullstack@163.com>
2018-09-09 16:05:25 +0800

    导入模型类，执行数据库迁移

--- info/__init__.py
+++ info/__init__.py
@@ -4,12 +4,17 @@ from config import config
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 
+#先实例化sqlalchemy对象
+db = SQLAlchemy()
+
 #设计模式:工厂函数  :让app通过函数进行调用,可以动态的生产不同状态的app
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    db = SQLAlchemy(app)
+    #让sqlalchemy对象和程序进行关联
+    db.init_app(app)    #传入app
+
     # 初始化session 和程序实例进行关联
     Session(app)
     return app
--- manage.py
+++ manage.py
@@ -1,16 +1,22 @@
 from flask import session
-from info import create_app
+from info import create_app,db,models
 
 #导入flask_script
 from flask_script import Manager
+
+#导入数据库迁移
+from flask_migrate import Migrate,MigrateCommand
 
 #调用生成的app，传入不同的配置信息
 app = create_app('development')
 
 #实例化管理对象
 manage = Manager(app)
+#此时db已经和程序进行关联了
+Migrate(app,db)
 
-
+#使用迁移框架
+manage.add_command("db",MigrateCommand)
 @app.route("/")
 def index():
 
